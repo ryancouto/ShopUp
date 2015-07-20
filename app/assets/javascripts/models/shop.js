a8105d52aff8e383dd3432b16de817df76e16db8
Ryan Couto <r4couto@gmail.com>
2015-07-20 14:56:57 -0400

    Set up shop-review association in backbone

--- app/assets/javascripts/models/shop.js
+++ app/assets/javascripts/models/shop.js
@@ -5,13 +5,24 @@ ShopUp.Models.Shop = Backbone.Model.extend({
     if (!this._reservations) {
       this._reservations = new ShopUp.Collections.Reservations([], { shop: this });
     }
-    return this._reservations
+    return this._reservations;
   },
+
+  reviews: function () {
+    if (!this._reviews) {
+      this._reviews = new ShopUp.Collections.Reviews([], { shop: this });
+    }
+    return this._reviews;
+  }
 
   parse: function (response) {
     if (response.reservations) {
       this.reservations().set(response.reservations, { parse: true });
       delete response.reservations;
+    }
+    if (response.reviews) {
+      this.reviews().set(response.reviews, { parse: true });
+      delete response.reviews;
     }
     return response
   }
